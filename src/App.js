314803b01d1de33afa3a689fbb3f5cefd7858fdf
Aplusblockchaindev <dcmanagertech1121@gmail.com>
2022-04-06 12:36:38 -0400

    auth0 social connection

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginButton from './components/LoginButton'
+
+export default function App() {
+  return (
+    <div>
+      <h1>React x Auth0</h1>
+      <LoginButton />
+    </div>
+  )
+}
---
+++ src/components/LoginButton.js
@@ -0,0 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import '../App.css'
+
+function LoginButton(props) {
+    const { loginWithRedirect } = useAuth0()
+    return (
+        <div>
+            <button 
+                onClick={()=>loginWithRedirect()} 
+                className="loginButton"
+                >
+                Login
+                </button>
+        </div>
+    )
+}
+
+export default LoginButton
